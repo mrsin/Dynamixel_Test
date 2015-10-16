c21693133732f82b90db8b23307d340b40c52bee
Sergey Sinitsa <info@drupal-coder.ru>
2015-10-16 19:37:23 +0300

    Documentation.

--- Dynamixel_Test.ino
+++ Dynamixel_Test.ino
@@ -2,10 +2,11 @@
 
 int Temperature,Voltage,Position; 
 
+// Broadcast servo ID. Factory default is 1.
 #define SERVO_ID 254
 
 void setup(){
-  Serial.begin(9600);              // Begin Serial Comunication
+  Serial.begin(9600);          // Begin Serial Comunication
   Dynamixel.begin(500000, 2);  // Inicialize the servo at 1Mbps and Pin Control 2
   delay(1000);
 }
@@ -14,9 +15,9 @@ void loop(){
   Temperature = Dynamixel.readTemperature(SERVO_ID); // Request and Print the Temperature
   Voltage = Dynamixel.readVoltage(SERVO_ID);         // Request and Print the Voltage
   Position = Dynamixel.readPosition(SERVO_ID);       // Request and Print the Position 
- 
+
   Dynamixel.move(SERVO_ID,random(200,800));  // Move the Servo radomly from 200 to 800
- 
+
   Serial.print(" *** Temperature: ");   // Print the variables in the Serial Monitor
   Serial.print(Temperature);
   Serial.print(" Celcius  Voltage: ");
@@ -24,6 +25,6 @@ void loop(){
   Serial.print("  Volts   Position: ");
   Serial.print(Position);
   Serial.println(" of 1023 resolution");
-  
+
   delay(1000);
 }
--- README.txt
+++ README.txt
@@ -1,1 +1,7 @@
- 
+Пример управляет и читает данные с сервы SPRINGRC SR508H по протоколу Dynamixel на Arduino Mega через библтотеку DynamixelSerial1.
+
+Схема подключения Dynamixel Servo через RS485:
+http://www.pablogindel.com/informacion/the-arduinodynamixel-resource-page/
+
+Библиотека Dynamixel для Arduino:
+http://savageelectrtonics.blogspot.com/
